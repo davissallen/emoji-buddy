3f29b209573fbc67b192df82c5f9c24764ab5cd6
Michael Cruz <michael@michaelcruz.io>
2018-05-28 17:17:47 -0700

    add main and configuration

---
+++ src/main/kotlin/io/michaelcruz/b2_httpd/B2HttpdConfiguration.kt
@@ -0,0 +1,10 @@
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.dropwizard.Configuration
+
+public class B2HttpdConfiguration() : Configuration() {
+    @JsonProperty("template")
+    public var template: String=""
+
+    @JsonProperty("defaultName")
+    public var defaultName: String="Stranger"
+}
